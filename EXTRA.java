99e2a97cf437d94a14b4dbceb2801b2657759995
prushni1 <jprushni@gmail.com>
2016-08-29 12:31:00 +0530

    Add files via upload

---
+++ EXTRA.java
@@ -0,0 +1,31 @@
+
+
+/*
+ * 
+ * 
+ * class example {
+ int x; int y;
+ String name;
+ public static void main(String args[]) {
+ example pnt = new example();
+ System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+} }
+ 
+ * 
+ * */
+public class EXTRA {
+
+	 int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+		 EXTRA pnt = new EXTRA();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	} 
+
+}
+
+/*
+ *OUTPUT IS :
+ *   pnt is null 0 0
+ *
+ */
